82acaddff76a2d3c281c2cabb5871d2dcefae036
Bibek Chitrakar <bibek.chitrakar@gmail.com>
2015-07-13 15:35:52 -0500

    q2 and q3

--- week4/Day1/Q2/AvoidNegative.java
+++ week4/Day1/Q2/AvoidNegative.java
@@ -1,26 +1,25 @@
 package week4.Day1.Q2;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class AvoidNegative {
-	
-	static int getMark() throws InputMismatchException {
-		Scanner sc = new Scanner(System.in);
-		int mark = sc.nextInt();
-		if(mark<0 || mark>100)
-			throw new Exception("Number Out of Range");
-		return mark;
-	}
 	
 	public static void main(String[] args) {
 		System.out.println("Enter mark in range 0 to 100...");
 		System.out.println("Marks: ");
 		try {
-			int mark = getMark();
+			Scanner sc = new Scanner(System.in);
+			int mark = sc.nextInt();
+			if(mark<0 || mark>100)
+				throw new Exception("Number Out of Range");
 			System.out.print("Your mark is " + mark);
 		}
+		catch(InputMismatchException e) {
+			System.out.println("Exception occurred: " + e.getMessage());
+		}
 		catch(Exception e){
-			System.out.println("Exception occurred: " + e);
+			System.out.println("Exception occurred: " + e.getMessage());
 		}
 		
 	}
---
+++ week4/Day1/Q3/Factorial.java
@@ -0,0 +1,7 @@
+package week4.Day1.Q3;
+
+public class Factorial {
+	public static void main(String[] args) {
+		
+	}
+}
