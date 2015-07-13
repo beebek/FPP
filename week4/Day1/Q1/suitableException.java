9c771da74c263564a748f770f89b98aaf46d8304
Bibek Chitrakar <bibek.chitrakar@gmail.com>
2015-07-13 17:50:20 -0500

    output added

--- week4/Day1/Q1/suitableException.java
+++ week4/Day1/Q1/suitableException.java
@@ -22,3 +22,55 @@ public class suitableException {
 		calculate();
 	}
 }
+
+// Output
+
+/*
+	4/2 = 2
+	8/2 = 4
+	16/2 = 8
+	32/2 = 16
+	64/2 = 32
+	128/2 = 64
+	256/2 = 128
+	512/2 = 256
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	4/4 = 1
+	8/4 = 2
+	16/4 = 4
+	32/4 = 8
+	64/4 = 16
+	128/4 = 32
+	256/4 = 64
+	512/4 = 128
+	4/4 = 1
+	8/4 = 2
+	16/4 = 4
+	32/4 = 8
+	64/4 = 16
+	128/4 = 32
+	256/4 = 64
+	512/4 = 128
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	java.lang.ArithmeticException: / by zero
+	4/8 = 0
+	8/8 = 1
+	16/8 = 2
+	32/8 = 4
+	64/8 = 8
+	128/8 = 16
+	256/8 = 32
+*/
