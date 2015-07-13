f1575dea222d4ce58ab8884f5bdd3ae5b66da577
Bibek Chitrakar <bibek.chitrakar@gmail.com>
2015-07-13 15:28:27 -0500

    second question

--- week4/Day1/Q2/AvoidNegative.java
+++ week4/Day1/Q2/AvoidNegative.java
@@ -4,17 +4,24 @@ import java.util.Scanner;
 
 public class AvoidNegative {
 	
-	static int getMark() throws Exception {
+	static int getMark() throws InputMismatchException {
 		Scanner sc = new Scanner(System.in);
 		int mark = sc.nextInt();
+		if(mark<0 || mark>100)
+			throw new Exception("Number Out of Range");
 		return mark;
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Enter mark in range 0 to 100...");
 		System.out.println("Marks: ");
-		int mark = getMark();
-		System.out.println("Your mark is " + mark);
+		try {
+			int mark = getMark();
+			System.out.print("Your mark is " + mark);
+		}
+		catch(Exception e){
+			System.out.println("Exception occurred: " + e);
+		}
 		
 	}
 
